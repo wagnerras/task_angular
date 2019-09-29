f4e2447eb66638aed760721c53316ac44ffbdbea
Wagner Ribeiro <wagnerras@gmail.com>
2019-09-29 18:32:18 -0300

    adicionando ao git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gerenciandor de tarefas';
+  
+  task: Task = {
+    id: 21,
+    title: "Levar o computador para consertar."
+  };
+
+  task2: Task = new Task(32, "Dar banho no cachorro.")
 }
+
+export class Task {
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
+ }
+ 
