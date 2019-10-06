18dd3897de9b8fac6a594e45f0b82d9e12c44962
Wagner Ribeiro <wagnerras@gmail.com>
2019-10-06 15:36:45 -0300

    adicionando promise

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -10,16 +10,21 @@ const TASKS: Array<Task> = [
   {id: 4, title: 'fazer tarefa 4'},
   {id: 5, title: 'fazer tarefa 5'},
   {id: 6, title: 'fazer tarefa 6'},
-  {id: 7, title: 'fazer tarefa 7'},
-  {id: 8, title: 'fazer tarefa 8'},
+  {id: 7, title: 'fazer tarefa 7'}
 ];
 
 @Injectable()
 
 export class TaskService{
 
-  public getTasks(): Array<Task> {
-    return TASKS;
+  public getTasks(): Promise<Array<Task>> {
+    return new Promise((resolve, reject) => {
+      if(TASKS.length > 0) {
+        resolve(TASKS);
+      }else{
+        reject("NÃO HÁ TAREFAS");
+      }     
+    })
   }
   
 }
--- src/app/tasks/task.component.ts
+++ src/app/tasks/task.component.ts
@@ -21,7 +21,9 @@ export class TasksComponent implements OnInit{
   };
 
   public ngOnInit(){
-    this.tasks = this.taskService.getTasks();
+   this.taskService.getTasks()
+    .then((tasks) => this.tasks = tasks)
+    .catch((error_msg) => console.log(error_msg));
   }
 
  public onSelect(task: Task) {
