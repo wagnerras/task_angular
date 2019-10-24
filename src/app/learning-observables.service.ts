5b12c5551abb53fa52860ec8b143d35f8539eee5
Wagner Ribeiro <wagnerras@gmail.com>
2019-10-24 17:04:20 -0300

    tratando errors

--- src/app/learning-observables.service.ts
+++ src/app/learning-observables.service.ts
@@ -1,76 +1,101 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Response } from "@angular/http";
+
+//tratando errors
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/catch";
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 
 export class LearningObservables {
-  constructor (private http: Http) {
+  constructor(private http: Http) {
 
     //criando um objeto observador
-/* 
-    let observer = {
-      next: function(newData) {
-        console.log("chamou o next e passou como parametro o new data => ", newData)
-      },
-
-      error: function(errorData) {
-        console.log("chamou o error passou como parametro o error data=> ", errorData)
-      },
-
-      complete: function() {
-        console.log("chamou o complete e encerrou")
-      }    
-    };
+    /* 
+        let observer = {
+          next: function(newData) {
+            console.log("chamou o next e passou como parametro o new data => ", newData)
+          },
     
-    //criando um objeto observado e passando umo observador como parametro
-     this.http.get("api/tasks")
-      .subscribe(observer); 
+          error: function(errorData) {
+            console.log("chamou o error passou como parametro o error data=> ", errorData)
+          },
+    
+          complete: function() {
+            console.log("chamou o complete e encerrou")
+          }    
+        };
+        
+        //criando um objeto observado e passando umo observador como parametro
+         this.http.get("api/tasks")
+          .subscribe(observer); 
+    
+        //criando um objeto observado e criando um observador como parametro 
+        this.http.get("api/tasks")
+          .subscribe({
+            next: function(newData) {
+              console.log("chamou o next e passou como parametro o new data => ", newData)
+            },
+      
+            error: function(errorData) {
+              console.log("chamou o error passou como parametro o error data=> ", errorData)
+            },
+      
+            complete: function() {
+              console.log("chamou o complete e encerrou")
+            }    
+          }); 
+    
+          //passando os metodes next, error e complete diretamente como parametros
+        this.http.get("api/tasks")
+          .subscribe(
+            function(newData) {
+              console.log("chamou o next e passou como parametro o new data => ", newData)
+            },
+            function(errorData) {
+              console.log("chamou o error passou como parametro o error data=> ", errorData)
+            },
+            function() {
+              console.log("chamou o complete e encerrou")
+            }    
+          )
+    
+          //passando os metodes next, error e complete diretamente como parametros
+          //usando arrow functions
+        this.http.get("api/tasks")
+        .subscribe(
+          (newData) => {
+            console.log("chamou o next e passou como parametro o new data => ", newData)
+          },
+          (errorData) => {
+            console.log("chamou o error passou como parametro o error data=> ", errorData)
+          },
+          () =>  console.log("chamou o complete e encerrou")      
+        ) */
 
-    //criando um objeto observado e criando um observador como parametro 
+    /* //passando apenas o metodo next
     this.http.get("api/tasks")
+    .subscribe(newData => console.log("chamou o next e passou como parametro o new data => ", newData)); */
+
+    this.http.get("api/tasksgfgfg")
+      .catch(this.handleErrors)
       .subscribe({
-        next: function(newData) {
+        next: (newData) => {
           console.log("chamou o next e passou como parametro o new data => ", newData)
         },
-  
-        error: function(errorData) {
-          console.log("chamou o error passou como parametro o error data=> ", errorData)
+        error: (errorData) => {
+          alert("ocorreu um error no servidor, por favor tente mais tarde.")
         },
-  
-        complete: function() {
+        complete: () => {
           console.log("chamou o complete e encerrou")
-        }    
-      }); 
-
-      //passando os metodes next, error e complete diretamente como parametros
-    this.http.get("api/tasks")
-      .subscribe(
-        function(newData) {
-          console.log("chamou o next e passou como parametro o new data => ", newData)
-        },
-        function(errorData) {
-          console.log("chamou o error passou como parametro o error data=> ", errorData)
-        },
-        function() {
-          console.log("chamou o complete e encerrou")
-        }    
-      )
-
-      //passando os metodes next, error e complete diretamente como parametros
-      //usando arrow functions
-    this.http.get("api/tasks")
-    .subscribe(
-      (newData) => {
-        console.log("chamou o next e passou como parametro o new data => ", newData)
-      },
-      (errorData) => {
-        console.log("chamou o error passou como parametro o error data=> ", errorData)
-      },
-      () =>  console.log("chamou o complete e encerrou")      
-    ) */
-
-    //passando apenas o metodo next
-    this.http.get("api/tasks")
-      .subscribe(newData => console.log("chamou o next e passou como parametro o new data => ", newData));
+        }
+      });
   }
+
+  public handleErrors(error: Response) {
+    console.log("SALVANDO O ERRO NO BANCO DE DADOS PARA O DEV =>", error);
+    return Observable.throw(error);
+  }
+
 }
