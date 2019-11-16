bc1e7466d0433d45e0a6ceb0b03b8244ed2c01b1
Wagner Ribeiro <wagnerras@gmail.com>
2019-11-16 15:45:42 -0300

    atualizando tasks

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers } from "@angular/http";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
@@ -36,6 +36,16 @@ export class TaskService {
     return this.http.get(url)
       .catch(this.handleErrors)
       .map((response: Response) => response.json().data as Task)
+  }
+
+  public updateTask (task: Task): Observable<Task>{
+      let url = `${this.tasksUrl}/${task.id}`;
+      let body = JSON.stringify(task);
+      let headers = new Headers({'Content-type': 'application/json' });
+
+      return this.http.put(url, body, { headers: headers})
+        .catch(this.handleErrors)
+        .map(() => task);
   }
 
   private handleErrors(error: Response) {
