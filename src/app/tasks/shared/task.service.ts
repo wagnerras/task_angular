932cde28fef743cb9d958df30bcb1d416e3258a6
Wagner Ribeiro <wagnerras@gmail.com>
2019-11-20 11:11:36 -0300

    deletando tarefas

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -58,6 +58,15 @@ export class TaskService {
         .map(() => task);
   }
 
+  public deleteTask(id: number): Observable<null> {
+    let url = `${this.tasksUrl}/${id}`;
+    let headers = new Headers({'Content-Type': 'applicationj/json'});
+
+    return this.http.delete(url, {headers: headers})
+      .catch(this.handleErrors)
+      .map(() => null )
+  }
+
   private handleErrors(error: Response) {
     console.log("DETALHES DO ERRO =>", error);
     return Observable.throw(error);
