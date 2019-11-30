e342cefe5198dfd9c18b3510051cc852618b9aaf
Wagner Ribeiro <wagnerras@gmail.com>
2019-11-30 17:32:11 -0300

    melhorando validacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 //angular importas
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, Validators } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 //components imports
