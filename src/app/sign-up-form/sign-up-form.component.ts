2f5f8432484d9721a7d0367b499fcbeb3abab14f
Wagner Ribeiro <wagnerras@gmail.com>
2020-01-18 11:18:32 -0300

    criando sign up component forms

---
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,26 @@
+import {Component} from "@angular/core";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'sign-up-form',
+  templateUrl: './sign-up-form.component.html'
+})
+
+export class SignUpFormComponent {
+  public userForm: FormGroup;
+
+  public constructor(private formBuilder: FormBuilder){
+    this.userForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(7), Validators.maxLength(100)]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      passwordConfirmation: [null, [Validators.required]]
+    })
+  }
+
+  public signUpUser(){
+    console.log("Formulário de SignUP enviado");
+    console.log(this.userForm.value);
+  }
+
+}
